bdca5047901bd70101bd0ff68fa5f03f27bdb02e
Mohammad Yeasin Ali <ys.niloy.666@gmail.com>
2026-04-29 23:04:36 +0600

    Folder 59

---
+++ Folder 60/index.js
@@ -0,0 +1,9 @@
+book = "You";
+category = "Dark psychology";
+
+console.log("The book " + book + " contains a lot of " + category);
+
+console.log(`The book ${book} contains a lot of ${category}`);
+
+
+
