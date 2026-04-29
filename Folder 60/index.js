b980b8245a7b6c719be649f07a71e2cb6608c46d
Mohammad Yeasin Ali <ys.niloy.666@gmail.com>
2026-04-29 23:32:39 +0600

    Folder 60 new file

--- Folder 60/index.js
+++ Folder 60/index.js
@@ -1,9 +1,20 @@
-book = "You";
-category = "Dark psychology";
+let book = "You";
+let category = "Dark psychology";
 
-console.log("The book " + book + " contains a lot of " + category);
+console.log("The book " + book + " contains a lot of " + category)
 
-console.log(`The book ${book} contains a lot of ${category}`);
+console.log(`The book ${book} contains a lot of ${category}`)
+
+let b = "Cut the string"
+
+console.log(b.toUpperCase())
+
+console.log(b.length)
+console.log(b.slice(1))
+console.log(b.slice(1,5))
+console.log(b.replace("Cut ", "okay"))
+
+
 
 
 
